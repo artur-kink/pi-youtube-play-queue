352d540d48b6ad69daf4834d8daffe383ee06e5a
artur.kink <artur.kink@gmail.com>
2013-09-06 03:06:26 -0400

    Mysql table script

---
+++ table.sql
@@ -0,0 +1,11 @@
+drop table history;
+
+create table history(
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	ip nvarchar(16),
+	video nvarchar(255),
+	title nvarchar(255),
+	duration nvarchar(10),
+	played bit default 0,
+	reqtime timestamp
+);
